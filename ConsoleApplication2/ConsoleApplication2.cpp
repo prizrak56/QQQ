1fe9d42075da423eec0f7d4923c614e63f534151
Ghost <prizrak9andrew@gmail.com>
2024-01-18 02:01:56 +0300

    overloading

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,271 +1,101 @@
-﻿#include <algorithm>
-#include <windows.h>
-#include <iostream>
-#include <stdlib.h>
-#include <utility>
-#include <conio.h> 
-#include <fstream>
-#include <string>
+﻿#include <iostream>
+#include <stdexcept>
 #include <vector>
-#include <cmath>
-#include <map>
-#include <set>
-#include <iomanip>
-#undef max
 
 using namespace std;
 
-long double OnlyNumbers(string x) {
-	string a;
-	for (char c : x) {
-		if (c >=0 || c<=9) {
-			a += c;
-		}
-	}
-	return stod(a);
-}
-
-void ColorText(int x) {
-	HANDLE console_color;
-	console_color = GetStdHandle(
-	STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(
-	console_color, x);
-}
-void ColorReturn() {
-	HANDLE console_color;
-	console_color = GetStdHandle(
-	STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(
-	console_color, 7);
-}
-
-class Accounting {
+class Tower {
 public:
-	
-	void SaveToFile(const string& filename) {
-		ofstream out_file(filename, ios::out);
-		if (!out_file.is_open()) {
-			cerr << "Could not open the file - '" << filename << "' for writing." << endl;
-			return;
-		}
+    // конструктор и метод SetDisks нужны, чтобы правильно создать башни
+    Tower(int disks_num) {
+        FillTower(disks_num);
+    }
 
-		out_file << balance_ << endl;
+    int GetDisksNum() const {
+        return disks_.size();
+    }
 
-		for (auto& inc : income_) {
-			out_file << "Inc " << inc.first << " " << inc.second << endl;
-		}
+    void SetDisks(int disks_num) {
+        FillTower(disks_num);
+    }
 
-		for (auto& exp : expenses_) {
-			out_file << "Exp " << exp.first << " " << exp.second << endl;
-		}
+    // добавляем диск на верх собственной башни
+    // обратите внимание на исключение, которое выбрасывается этим методом
+    void AddToTop(int disk) {
+        int top_disk_num = disks_.size() - 1;
+        if (0 != disks_.size() && disk >= disks_[top_disk_num]) {
+            throw invalid_argument("Невозможно поместить большой диск на маленький");
+        }
+        else {
+            // допишите этот метод и используйте его в вашем решении
+        }
+    }
 
-		out_file.close();
-	}
-
-	void LoadFromFile(const string& filename) {
-		ifstream in_file(filename, ios::in);
-
-		if (!in_file.is_open()) {
-			return;
-		}
-
-		in_file >> balance_;
-
-		string type, target;
-		int sum;
-
-		while (in_file >> type >> target >> sum) {
-			if (type == "Inc") {
-				income_.push_back({ target, sum });
-				history_.push_back({ target, {sum, "Income"} });
-			}
-			else if (type == "Exp") {
-				expenses_.push_back({ target, sum });
-				history_.push_back({ target, {sum, "Expenses"} });
-			}
-		}
-
-		in_file.close();
-	}
-
-	void ShowBalance () {
-		ColorText(10);
-		cout << "Your balance at the moment: " <<fixed << setprecision(2) << balance_ << "\n" << endl;
-		ColorReturn();
-	}
-
-	void ShowIncome() {
-		int operationIndex = -1;
-		cout << "Выберите операцию чтобы узнать сумму пополнения.\n" << endl;
-		cout << endl;
-		ColorText(14);
-		for (int i = 0; i < income_.size(); i++) {
-			cout << income_[i].first << " введите -> " << i << "." << endl;
-		}
-		cout << "\nЧтобы посмоитерь все операции дохода. Введите -> -1." << endl;
-		cout << endl;
-		ColorReturn();
-		cin >> operationIndex;
-		cout << endl;
-		ColorText(10);
-		if (operationIndex <= income_.size() && operationIndex >= 0 && operationIndex != -1) {
-			cout << "Income " << income_[operationIndex].second << endl;
-		}
-		else if (operationIndex == -1) {
-			for (auto &i : income_) {
-				cout << i.first << " " << i.second << endl;
-			}
-		}
-		else if (expenses_.empty() || operationIndex < -1) {
-			ColorText(12);
-			cout << "no operations" << endl;
-		}
-		cout << endl;
-		ColorReturn();
-	}
-
-	void ShowExpenses() {
-		int operationIndex = -1;
-		cout << "Выберите операцию чтобы узнать сумму трат.\n" << endl;
-		ColorText(14);
-		for (int i = 0; i < expenses_.size(); i++) {
-			cout << expenses_[i].first << " введите -> " << i << "." << endl;
-		}
-		cout << "\nЧтобы посмоитерь все операции трат. Введите -> -1" << endl;
-		cout << endl;
-		ColorReturn();
-		cin >> operationIndex;
-		cout << endl;
-		ColorText(10);
-		if (operationIndex <= expenses_.size() && operationIndex >= 0 && operationIndex != -1) {
-			cout << expenses_[operationIndex].first << " " << expenses_[operationIndex].second << endl;
-		}
-		else if (operationIndex == -1) {
-			for (auto &i : expenses_) {
-				cout << i.first << " " << i.second << endl;
-			}
-		}
-		else if (expenses_.empty() || operationIndex < -1) {
-			ColorText(12);
-			cout << "no operations" << endl;
-		}
-		cout << endl;
-		ColorReturn();
-	}
-
-	void EditIncome(string date, long double sum) {
-		balance_ += sum;
-		history_.push_back({date,{sum,"Income"}});
-		income_.push_back({date,sum});
-	}
-
-	void EditExpenses(string date, long double sum) {
-
-		balance_ -= sum;
-		history_.push_back({date,{sum,"Expenses"}});
-		expenses_.push_back({date,sum});
-
-	}
-	void ShowAllHistory() {
-
-		ColorText(10);
-		for (auto &i : history_) {
-			cout << i.first << " " << i.second.first << " " << i.second.second << endl;
-		}
-		ColorReturn();
-	}
+    // вы можете дописывать необходимые для вашего решения методы
 
 private:
-	long double balance_ = 0;
-	vector < pair < string, pair < long double, string >> > history_;
-	vector<pair<string, long double>> income_;
-	vector<pair<string, long double>> expenses_;
+    vector<int> disks_;
+
+    // используем приватный метод FillTower, чтобы избежать дубликации кода
+    void FillTower(int disks_num) {
+        for (int i = disks_num; i > 0; i--) {
+            disks_.push_back(i);
+        }
+    }
 };
+
+void SolveHanoi(vector<Tower>& towers) {
+    int disks_num = towers[0].GetDisksNum();
+
+    // допишите функцию, чтобы на towers[0] было 0 дисков,
+    // на towers[1] 0 дисков,
+    // и на towers[2] было disks_num дисков
+}
 
 
 int main() {
-	setlocale(0, "");
-	Accounting one;
-	one.LoadFromFile("accounting.txt");
-	string edit;
-	string target;
-	int vote = 0;
-	while (vote != -1) {
-		string ie;
-		cout << " Select an action! \n 1 - Show balance \n 2 - Edit expenses \n 3 - Edit income \n 4 - history exp/inc \n 5 - all history \n-1 - exit\n" << endl;
-		cin >> vote;
-		cout << endl;
-		switch (vote)
-		{
-		
-		case 1:
-			system("cls");
-			one.ShowBalance();
-			break;
+    int towers_num = 3;
+    int disks_num = 3;
+    vector<Tower> towers;
+    // Добавим в вектор три пустые башни.
+    for (int i = 0; i < towers_num; ++i) {
+        towers.push_back(0);
+    }
+    // Добавим на первую башню три кольца.
+    towers[0].SetDisks(disks_num);
 
-		case 2:
-			system("cls");
-			cout << " Enter the purpose of the spending and then the amount of the spending.\n\n Target: ";
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			getline(cin, target);
-			cout << " Sum: ";
-			cin >> edit;
-			cout << endl;
-			one.EditExpenses(target, OnlyNumbers(edit));
-			ColorText(10);
-			cout << "Changes applied." << endl;
-			cout << endl;
-			ColorReturn();
-			break;
-
-		case 3:
-			system("cls");
-			cout << " Specify the purpose of the replenishment, and then the amount of income.\n\n Target: ";
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			getline(cin, target);
-			cout << " Sum: ";
-			cin >> edit;
-			cout << endl;
-			one.EditIncome(target, OnlyNumbers(edit));
-			ColorText(10);
-			cout << "Changes applied." << endl;
-			cout << endl;
-			ColorReturn();
-			break;
-
-		case 4:
-			system("cls");
-			cout << "what do you want to watch? (expenses or income) pls enter" << endl;
-			cin >> ie;
-			cout << endl;
-			if (ie == "income" || ie == "Income") {
-				one.ShowIncome();
-			}
-			else if(ie == "Expenses" || ie == "expenses") {
-				one.ShowExpenses();
-			}
-
-			break;
-
-		case 5:
-
-			system("cls");
-			one.ShowAllHistory();
-			cout << endl;
-			break;
-
-		default:
-			system("cls");
-			if (vote != -1) {
-				ColorText(12);
-				cout << "error" << endl;
-				ColorReturn();
-			}
-			break;
-		}
-	}
-	one.SaveToFile("accounting.txt");
-
-    return 0;
+    SolveHanoi(towers);
 }
+
+
+
+/*  Подсказка
+
+1. Какие параметры принимает функция?
+Здесь это самое интересное. Функция SolveHanoi сама по себе рекурсивной не будет. Используйте другую. Вам пригодится это:
+
+ // disks_num - количество перемещаемых дисков
+ // destination - конечная башня для перемещения
+ // buffer - башня, которую нужно использовать в качестве буфера для дисков
+ void MoveDisks(int disks_num, Tower& destination, Tower& buffer) {
+     if (// условие выхода ещё не выполнено) {
+         // действия из шага рекурсии
+     }
+ }
+
+ void SolveHanoi(vector<Tower>& towers) {
+     int disks_num = towers[0].GetDisksNum();
+     // запускаем рекурсию
+     // просим переложить все диски на последнюю башню
+     // с использованием средней башни как буфера
+     towers[0].MoveDisks(disks_num, towers[2], towers[1]);
+ }
+
+2. Какие действия должна совершить функция на каждом шаге?
+Препарируйте задачу. Порисуйте диски, найдите повторяющийся паттерн. Скорее всего, он будет состоять из нескольких действий.
+
+
+3. Каково условие завершения? 
+Всё просто — как только дисков не осталось, не нужно ничего больше перемещать.
+
+*/
